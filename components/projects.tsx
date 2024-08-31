ff8bbabd249204b9fd5489e42e2cc8f540029eba
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-08-31 19:50:26 +0900

    feat: HTML 언어 속성을 업데이트하고 body 요소에 부드러운 스크롤링 클래스를 추가함

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,8 +5,8 @@ import Project from "./project";
 
 export default function Projects() {
   return (
-    <section>
-      <SectionHeading>My Projects</SectionHeading>
+    <section id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
