1ba5363a71cbc6a1bc5aafc79c13275096dfe99d
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-09-27 10:27:51 +0900

    refactor: 명명된 함수 컴포넌트로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+// import About from "@/components/about";
+// import Contact from "@/components/contact";
+// import Education from "@/components/education";
+// import Intro from "@/components/intro";
+// import Projects from "@/components/projects";
+// import SectionDivider from "@/components/section-divider";
+// import Skills from "@/components/skills";
+
+// export default function () {
+//   return (
+//     <main className="flex flex-col items-center px-4">
+//       <Intro />
+//       <SectionDivider />
+//       <About />
+//       <Skills />
+//       <Projects />
+//       <Education />
+//       <Contact />
+//     </main>
+//   );
+
+// }
+
 import About from "@/components/about";
 import Contact from "@/components/contact";
 import Education from "@/components/education";
@@ -6,7 +29,7 @@ import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
-export default function () {
+const HomePage = () => {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
@@ -18,4 +41,6 @@ export default function () {
       <Contact />
     </main>
   );
-}
+};
+
+export default HomePage;
